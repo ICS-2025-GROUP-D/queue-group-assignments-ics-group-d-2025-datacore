39f7504a82f7b0403ff311c06ac68dc07cb4fe82
MARK <otingamark@gmail.com>
2025-06-20 08:34:38 +0300

    FIRST COMMIT

--- print_queue_manager.py
+++ print_queue_manager.py
@@ -3,22 +3,6 @@ class PrintQueueManager:
         self.queue = []
         self.time = 0
 
-    def tick(self):
-        self.time += 1
-        for job in self.queue:
-            job['waiting_time'] += 1
-        self.apply_priority_aging()
-        self.remove_expired_jobs()
-
-    def apply_priority_aging(self):
-        for job in self.queue:
-            if job['waiting_time'] % 3 == 0 and job['waiting_time'] != 0:
-                job['priority'] += 1
-
-    def remove_expired_jobs(self):
-        max_wait_time = 10
-        self.queue = [job for job in self.queue if job['waiting_time'] <= max_wait_time]
-
     def enqueue_job(self, user_id, job_id, priority):
         job = {
             'user_id': user_id,
@@ -28,7 +12,18 @@ class PrintQueueManager:
         }
         self.queue.append(job)
 
+    def tick(self):
+        self.time += 1
+        for job in self.queue:
+            job['waiting_time'] += 1
+
+        for job in self.queue:
+            if job['waiting_time'] % 3 == 0 and job['waiting_time'] != 0:
+                job['priority'] += 1
+
+        max_wait = 10
+        self.queue = [job for job in self.queue if job['waiting_time'] <= max_wait]
+
     def show_status(self):
         sorted_queue = sorted(self.queue, key=lambda job: (-job['priority'], job['waiting_time']))
-        for job in sorted_queue:
-            print(f"User: {job['user_id']}, Job: {job['job_id']}, Priority: {job['priority']}, Waiting: {job['waiting_time']}")
+        return sorted_queue
