e52c3ac05e8461e767dfce3c6fac8747e1188a13
MARK <otingamark@gmail.com>
2025-06-20 17:51:33 +0300

    first commit

--- print_queue_manager.py
+++ print_queue_manager.py
@@ -1,34 +1,13 @@
-class PrintQueueManager:
-    def __init__(self):
-        self.queue = []
-        self.time = 0
+def tick(self):
+    self.time += 1
 
-    def enqueue_job(self, user_id, job_id, priority):
-        job = {
-            "user_id": user_id,
-            "job_id": job_id,
-            "priority": priority,
-            "waiting_time": 0
-        }
-        self.queue.append(job)
+    for job in self.queue:
+        job["waiting_time"] += 1
 
-    def tick(self):
-        self.time += 1
+    for job in self.queue:
+        if job["waiting_time"] % self.aging_threshold == 0:
+            job["priority"] += 1  # or -= 1 if lower number = higher priority
 
-        for job in self.queue:
-            job["waiting_time"] += 1
-
-        for job in self.queue:
-            if job["waiting_time"] % 3 == 0:
-                job["priority"] += 1
-
-        self.queue = [
-            job for job in self.queue if job["waiting_time"] <= 10
-        ]
-
-    def show_status(self):
-        sorted_jobs = sorted(
-            self.queue,
-            key=lambda job: (-job["priority"], job["waiting_time"])
-        )
-        return sorted_jobs
+    self.queue = [
+        job for job in self.queue if job["waiting_time"] <= self.expiry_time
+    ]
